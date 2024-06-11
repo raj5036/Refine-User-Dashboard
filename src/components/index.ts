c2a0ac2d5b986d841ac2e3853f272940fc34c54b
Raj Karmakar <97rajmath@gmail.com>
2024-06-11 10:59:40 +0530

    Added Loading Indicator from SpinKit

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { Header } from "./header";
+import { LoadingIndicator } from "./LoadingIndicator/LoadingIndicator";
+
--- src/pages/users/list.tsx
+++ src/pages/users/list.tsx
@@ -1,9 +1,18 @@
 import { useTable } from "@refinedev/antd";
 import React from "react";
 import { IUser } from "../../interfaces";
+import { LoadingIndicator } from "../../components/LoadingIndicator/LoadingIndicator";
 
 export const UserList: React.FC = () => {
 	const { tableProps } = useTable<IUser>({syncWithLocation: true})
 	console.log(tableProps)
-	return <div>UserList</div>;
+	return (
+		<React.Fragment>
+			{tableProps.loading ? 
+				<LoadingIndicator/> : (
+					'UserList'
+				)}
+		</React.Fragment>
+	)
+	
 }
