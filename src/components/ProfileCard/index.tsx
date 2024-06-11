8e15484d371d91dd797342d3c8a4d6ada503801a
Raj Karmakar <97rajmath@gmail.com>
2024-06-11 16:45:18 +0530

    Added Antd Modal for user edit

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -6,6 +6,7 @@ import { DicebarAPI_URL } from "../../utils";
 import "./index.css"
 import { DeleteButton, EditButton } from "@refinedev/antd";
 import { GlobalOutlined, HeartFilled, HeartOutlined, MailOutlined, PhoneOutlined } from "@ant-design/icons";
+import { UserEdit } from "../../pages/users";
 
 interface PropTypes {
 	user: IUser
@@ -13,34 +14,43 @@ interface PropTypes {
 
 export const ProfileCard: React.FC<PropTypes> = ({ user }) => {
 	const [liked, setLiked] = useState<boolean>(false);
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
 	const onLikeButtonClick = () => {
 		setLiked(!liked);
 	}
 
+	const onEditButtonClick = () => {
+		setIsModalOpen(true)
+	}
+
 	return (
-		<div className="profileCardContainer">
-			<img 
-				src={DicebarAPI_URL + user.username} 
-				alt={user.username + "avatar"}
-				className="profileCardImage"
-			/>
-			<div className="profileInfoContainer">
-				<p className="profileName">{user.name}</p>
-				<div><MailOutlined /> {user.email}</div>
-				<div><PhoneOutlined /> {user.phone}</div>
-				<div><GlobalOutlined /> {user.website}</div>
+		<React.Fragment>
+			{isModalOpen && <UserEdit isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>}
+			<div className="profileCardContainer">
+				<img 
+					src={DicebarAPI_URL + user.username} 
+					alt={user.username + "avatar"}
+					className="profileCardImage"
+				/>
+				<div className="profileInfoContainer">
+					<p className="profileName">{user.name}</p>
+					<div><MailOutlined /> {user.email}</div>
+					<div><PhoneOutlined /> {user.phone}</div>
+					<div><GlobalOutlined /> {user.website}</div>
+				</div>
+				<div className="actionButtonsContainer">
+					{!liked ? <HeartOutlined 
+						className="actionButtons"
+						onClick={onLikeButtonClick}/> 
+						: <HeartFilled 
+							className="actionButtons liked"
+							onClick={onLikeButtonClick}
+						/>}
+					<EditButton hideText size="small" className="actionButtons" onClick={onEditButtonClick}/>
+					<DeleteButton hideText size="small" recordItemId={user.id} className="actionButtons"/>
+				</div>
 			</div>
-			<div className="actionButtonsContainer">
-				{!liked ? <HeartOutlined 
-					className="actionButtons"
-					onClick={onLikeButtonClick}/> 
-					: <HeartFilled 
-						className="actionButtons liked"
-						onClick={onLikeButtonClick}
-					/>}
-				<EditButton hideText size="small" recordItemId={user.id} className="actionButtons"/>
-				<DeleteButton hideText size="small" recordItemId={user.id} className="actionButtons"/>
-			</div>
-		</div>
+		</React.Fragment>
 	);
 }
--- src/pages/users/edit.tsx
+++ src/pages/users/edit.tsx
@@ -1,5 +1,19 @@
+import { Modal } from "antd";
 import React from "react";
 
-export const UserEdit: React.FC = () => {
-	return <div>UserEdit</div>;
+interface PropTypes {
+	isModalOpen: boolean,
+	setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+export const UserEdit: React.FC<PropTypes> = ({ isModalOpen, setIsModalOpen }) => {
+
+	const handleCancel = () => {
+		setIsModalOpen(false);
+	};
+
+	return (
+		<Modal title="Basic Modal" open={isModalOpen} onCancel={handleCancel}>
+			UserEdit
+		</Modal>
+	);
 }
