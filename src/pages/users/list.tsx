9b9140868fd15bbc4c9a1911ef4199830b9d3d0c
Raj Karmakar <97rajmath@gmail.com>
2024-06-12 10:46:25 +0530

    Grid Gutter span updated

--- src/pages/users/list.tsx
+++ src/pages/users/list.tsx
@@ -6,13 +6,13 @@ import { LoadingIndicator, ProfileCard } from "../../components";
 
 export const UserList: React.FC = () => {
 	const { tableProps } = useTable<IUser>({syncWithLocation: true})
-	// console.log(tableProps)
+	
 	return (
 		<React.Fragment>
 			{tableProps.loading ? 
 				<LoadingIndicator /> : (
 					<div style={{overflowX: "hidden"}}>
-						<Row gutter={[64, { xs: 8, sm: 16, md: 24, lg: 32 }]}>
+						<Row gutter={[16, { xs: 16, sm: 16, md: 24, lg: 24 }]}>
 							{tableProps?.dataSource?.map((user) => (
 								<Col key={user.id} xs={24} sm={12} md={8} lg={8} xl={6} xxl={6}>
 									<ProfileCard user={user}/>
